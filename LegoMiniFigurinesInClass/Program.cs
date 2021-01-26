dd64c0601c8d6e811989e31d5e3e4dffba6b1721
Chris LoJacono <clojacono92@gmail.com>
2021-01-25 19:37:58 -0600

    enums

---
+++ LegoMiniFigurinesInClass/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LegoMiniFigurinesInClass
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
